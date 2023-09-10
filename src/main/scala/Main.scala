6d1aeacfb799e50e55b06567275024b322a2c0f6
Konrad Wieczorek <konrad.wieczorek@playbookengineering.com>
2023-09-10 20:40:20 +0200

    removed main

--- src/main/scala/Main.scala
+++
@@ -1,5 +0,0 @@
-import plugin.Splitter
-
-object Main extends App {
-  println(Splitter.split("In 1991, while studying computer science at University of Helsinki, Linus Torvalds began a project that later became the Linux kernel. He wrote the program specifically for the hardware he was using and independent of an operating system because he wanted to use the functions of his new PC with an 80386 processor. Development was done on MINIX using the GNU C Compiler.", 40))
-}
